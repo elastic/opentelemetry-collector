7a8cae63b1acc01491572f27e1b2c1d20282d2fc
Vishal Raj <vishal.raj@elastic.co>
2025-10-14 14:11:04 +0100

    Adapt context propogation patch to v0.137.0

--- exporter/exporterhelper/internal/queuebatch/batch_context.go
+++ exporter/exporterhelper/internal/queuebatch/batch_context.go
@@ -5,13 +5,25 @@ package queuebatch // import "go.opentelemetry.io/collector/exporter/exporterhel
 
 import (
 	"context"
+	"slices"
 
+	"go.opentelemetry.io/collector/client"
 	"go.opentelemetry.io/otel/trace"
 )
 
 type traceContextKeyType int
 
 const batchSpanLinksKey traceContextKeyType = iota
+
+// allowedContextKeys is a hardcoded list of context keys that are added
+// to the context propogated by the batcher. This is an interim solution
+// until https://github.com/open-telemetry/opentelemetry-collector/issues/13320
+// is resolved and is kept simple for better maintainability across updates.
+var allowedContextKeys = []string{
+	"x-elastic-project-id",
+	"x-elastic-target-id",
+	"x-elastic-target-type",
+}
 
 // LinksFromContext returns a list of trace links registered in the context.
 func LinksFromContext(ctx context.Context) []trace.Link {
@@ -32,8 +44,22 @@ func parentsFromContext(ctx context.Context) []trace.Link {
 }
 
 func contextWithMergedLinks(mergedCtx, ctx1, ctx2 context.Context) context.Context {
+	meta1 := client.FromContext(ctx1).Metadata
+	meta2 := client.FromContext(ctx2).Metadata
+	m := make(map[string][]string)
+	for _, ak := range allowedContextKeys {
+		v1 := meta1.Get(ak)
+		v2 := meta2.Get(ak)
+		if len(v1) == 0 && len(v2) == 0 {
+			continue
+		}
+		if !slices.Equal(v1, v2) {
+			panic("unexpected metadata keys, the partition has allowed metadata keys with different values")
+		}
+		m[ak] = slices.Clone(v1)
+	}
 	return context.WithValue(
-		mergedCtx,
+		client.NewContext(mergedCtx, client.Info{Metadata: client.NewMetadata(m)}),
 		batchSpanLinksKey,
 		append(parentsFromContext(ctx1), parentsFromContext(ctx2)...))
 }
